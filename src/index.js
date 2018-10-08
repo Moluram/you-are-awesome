8a5da307c9fa24045c56a5f0cabfdf14d69fa5a6
achorny <moluram.a@gmail.com>
2018-10-08 12:07:57 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,18 +1,83 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (key) => {
+    return key
+};
+
+const createNotEnumerableProperty = (key) => {
+    return Symbol(key);
+};
+
+const createProtoMagicObject = () => {
+    function f() {}
+
+    f.__proto__ = f.prototype;
+    return f;
+};
+
+let count = 0;
+
+const incrementor = () => {
+    let innerIncrementor = () => {
+        ++count;
+        return innerIncrementor
+    };
+
+    innerIncrementor.valueOf = incrementor.valueOf = function () {
+        return ++count;
+    };
+
+    return innerIncrementor;
+};
+
+let aCount = 0;
+
+const asyncIncrementor = () => {
+    return new Promise((resolve) => {
+        aCount++;
+        return resolve(aCount);
+    });
+};
+
+const createIncrementer = () => {
+    return {
+        count: 0,
+        next() {
+            return {
+                value: ++this.count
+            }
+        },
+        [Symbol.iterator]() {
+            return {
+                next: () => {
+                    return this.next();
+                },
+            };
+        },
+    };
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (obj) => {
+    return new Promise((resolve) => {
+        setTimeout(function () {
+            resolve(obj);
+        }, 1010);
+    });
+};
+
+const getDeepPropertiesCount = (obj) => {
+    let properties = Object.getOwnPropertyNames(obj);
+    return properties.filter(p => Object.getOwnPropertyNames(obj[p]).length > 0)
+        .reduce((a, prop) => a + getDeepPropertiesCount(obj[prop]), properties.length);
+};
+
+const createSerializedObject = () => {
+    return new String()
+};
+
+const sortByProto = (arr) =>  arr.sort((a, b) => a.__proto__ - b.__proto__);
+
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
